b3790e12f793da2a88b16c9bcaf4a890964de491
Adrianus Vian Habirowo <vian@idjo.cc>
2026-01-28 16:09:05 +0700

    chore: 🧑‍💻 add nix flake dev environment

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "Resume";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            venvDir = ".venv";
+
+            packages = [
+              pkgs.kubernetes-helm
+              pkgs.yq-go
+              pkgs.pandoc
+              pkgs.texliveSmall
+            ];
+
+            shellHook = ''
+              export LD_LIBRARY_PATH=${
+                pkgs.lib.makeLibraryPath [
+                  pkgs.stdenv.cc.cc.lib
+                  pkgs.libz
+                ]
+              }
+              source .venv/bin/activate
+            '';
+          };
+        }
+      );
+    };
+}
